127c5d8348915c667f5712bc790ed6ce2abecb80
Denali Balser <denali.balser167@gmail.com>
2019-05-22 10:56:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,14 +2,15 @@ class School
   def initialize(name)
     @name = name
     ROSTER = {}
+    ROSTER["new_key"] = []
+    ROSTER["new_key"] << "new_value_for_value_array"
+    ROSTER => {"new_key"=>["new_value_for_value_array"]}
 
   end
 
   def add_student=(add_student)
     @add_student = add_student
-    ROSTER["new_key"] = []
-    ROSTER["new_key"] << "new_value_for_value_array"
-    ROSTER => {"new_key"=>["new_value_for_value_array"]}
+    
   end
 
   def add_student
